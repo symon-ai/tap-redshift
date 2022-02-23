f27f826c0821912cd26482c7b533fc22863a61b4
Mike van Lammeren <mvanlammeren@varicent.com>
2022-02-22 19:10:08 -0500

    WP-7737 no binary available for v2.7.3.2, but v2.7.4 is apparently there

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'pendulum==1.2.0',
         'singer-python==5.0.4',
         'backoff==1.3.2',
-        'psycopg2-binary==2.7.3.2',
+        'psycopg2-binary==2.7.4',
       ],
       setup_requires=[
         'pytest-runner>=2.11,<3.0a',
