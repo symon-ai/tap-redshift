73e6662408e15178d74c3c7ee7489f8a8ddb63f0
jay-varicent <jama@varicent.com>
2023-10-17 18:40:32 -0400

    WP-17438 fix err msg to be consistent

--- tap_redshift/__init__.py
+++ tap_redshift/__init__.py
@@ -250,7 +250,7 @@ def open_connection(config):
     except psycopg2.OperationalError as e:
         message = str(e)
         if 'password authentication failed for user' in message:
-            raise SymonException('The username and password provided are incorrect. Please try again.', 'odbc.AuthenticationFailed')
+            raise SymonException('The username or password provided is incorrect. Please check and try again.', 'odbc.AuthenticationFailed')
         if f'database "{config["dbname"]}" does not exist' in message:
             raise SymonException(f'The database "{config["dbname"]}" does not exist. Please ensure it is correct.', 'odbc.DatabaseDoesNotExist')
         if f'could not translate host name "{config["host"]}" to address' in message:
